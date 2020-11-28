6bc5bf12106a7878eae4ada46645ca817a05483e
rubyruins <soumya.parekh@somaiya.edu>
2020-11-29 01:33:12 +0530

    cached expensive functions

--- sentiment.py
+++ sentiment.py
@@ -3,20 +3,55 @@ import pandas as pd
 import plotly.express as px
 import random
 
-st.title('sentifluent.') 
-st.markdown('A dashboard for viewing story stats and character targeted sentiment analysis.')
-st.sidebar.markdown('[![Github Badge](https://img.shields.io/badge/-github-grey?style=for-the-badge&logo=github&logoColor=white&link=https://github.com/rubyruins/)](https://www.github.com/rubyruins/) [![Wattpad Badge](https://img.shields.io/badge/-wattpad-orange?style=for-the-badge&logo=wattpad&logoColor=white&link=https://www.wattpad.com/user/rubyruins)](https://www.wattpad.com/user/rubyruins)')
-st.sidebar.title('Pick a story.')
-story = st.sidebar.selectbox('Story', ['Crown of Glass', 'Queen of Death'])
+@st.cache(persist = True, suppress_st_warning = True)
+def load_data(my_story):
+	print("in load data")
+	if my_story == 'Crown of Glass' or not my_story:
+		my_data = pd.read_csv("Data/cog.csv")
+		my_names = ['Edwina', 'Amphitrite', 'Eric', 'Tristan', 'Drusilla', 'Aidon', 'Celestina', 'Deimos', 'Cosmo', 'Emerick', 'Thanatos', 'Ambrosine', 'Apollo', 'Titania', 'Favian', 'Vivian', 'Lucius', 'Eros', 'Elodie', 'Miriel', 'Helios']
+		my_defaults = ['Edwina', 'Vivian', 'Emerick', 'Amphitrite', 'Cosmo', 'Tristan']
+	else: 
+		my_data = pd.read_csv("Data/qod.csv")
+		my_names = ['Persephone', 'Hades', 'Demeter', 'Zeus', 'Hecate', 'Athena', 'Artemis', 'Thanatos', 'Poseidon', 'Charon', 'Cerberus']
+		my_defaults = ['Persephone', 'Hades', 'Demeter', 'Zeus', 'Hecate', 'Artemis']
+	my_data.pop('Unnamed: 0')
+	return my_data, my_names, my_defaults
 
-@st.cache(persist = True)
-def load_data():
-	cog = pd.read_csv("Data/cog.csv")
-	qod = pd.read_csv("Data/qod.csv")
-	cog.pop('Unnamed: 0')
-	qod.pop('Unnamed: 0')
-	return cog, qod
+@st.cache(suppress_st_warning = True)
+def person_to_sentiments(person):
+	df = data[data['Text'].str.contains(person.lower())]
+	pos = df[(df.Compound > 0)].groupby('Chapter Name').count().reindex(data['Chapter Name'].unique()).reset_index().fillna(0).Positive
+	neu = df[(df.Compound == 0)].groupby('Chapter Name').count().reindex(data['Chapter Name'].unique()).reset_index().fillna(0).Neutral
+	neg = df[(df.Compound < 0)].groupby('Chapter Name').count().reindex(data['Chapter Name'].unique()).reset_index().fillna(0).Negative
+	df = pd.DataFrame(columns = ['Positive', 'Neutral', 'Negative', 'Chapter Name'])
+	df['Chapter Name'] = data['Chapter Name'].unique()
+	df['Positive'] = pos
+	df['Negative'] = neg
+	df['Neutral'] = neu
+	df = df.sum(axis = 0).reset_index()[:3]
+	df.rename(columns ={'index': 'Sentiment', 0: 'Comments'}, inplace = True)
+	n = list(df.Sentiment.values)
+	v = list(df.Comments.values)
+	c = ['#636EFA', '#00CC96','#EF553B']
+	v, n, c = [list(i) for i in zip(*sorted(zip(v, n, c), reverse=True))]
+	return n, v, c
 
+@st.cache(suppress_st_warning = True)
+def people_to_sentiments(people):
+	temp = pd.DataFrame(columns = ['Character', 'Positive', 'Neutral', 'Negative'])
+	for person in people:
+		df = data[data['Text'].str.contains(person.lower())]
+		df = sentiments_to_chapter(df)
+		df = df.sum(axis = 0)
+		row = pd.Series(list([person, df.Positive, df.Neutral, df.Negative]), index=temp.columns)
+		temp = temp.append(row, ignore_index=True) 
+	temp[['Positive', 'Neutral', 'Negative']] = temp[['Positive', 'Neutral', 'Negative']].div(temp[['Positive', 'Neutral', 'Negative']].sum(axis=1), axis=0) * 100
+	temp[['Positive', 'Neutral', 'Negative']] = round(temp[['Positive', 'Neutral', 'Negative']], 2).astype(str) + '%' 
+	temp = pd.melt(temp, id_vars=['Character'], value_vars=['Positive', 'Negative', 'Neutral'])
+	temp.rename(columns ={'variable': 'Sentiment', 'value': '% Comments'}, inplace = True)
+	return temp
+
+@st.cache(suppress_st_warning = True)
 def sentiments_to_chapter(df):
 	pos = df[(df.Compound > 0)].groupby('Chapter Name').count().reindex(data['Chapter Name'].unique()).reset_index().fillna(0).Positive
 	neu = df[(df.Compound == 0)].groupby('Chapter Name').count().reindex(data['Chapter Name'].unique()).reset_index().fillna(0).Neutral
@@ -28,6 +63,7 @@ def sentiments_to_chapter(df):
 	df['Neutral'] = neu
 	return df
 
+@st.cache(suppress_st_warning = True)
 def sentiments_to_time(df, choice):
 	mydict = dict()
 	df = df.reset_index()
@@ -48,7 +84,7 @@ def sentiments_to_time(df, choice):
 		mydict = pd.DataFrame(list(mydict.items()),columns = ['Date','Negative Comments']).sort_values(by = 'Date')
 	return mydict
 
-def load_random(df):
+def load_random():
 	while True:
 		i = random.randint(0, len(data) - 1)
 		if 'This comment may be offensive.' in data.loc[i]['Original'] or len(data.loc[i]['Original']) < 100:
@@ -59,47 +95,24 @@ def load_random(df):
 			st.sidebar.markdown(f"{data.loc[i]['Original']}")
 			break
 
-cog, qod = load_data()
-
-if story == 'Crown of Glass':
-	data = cog
-	names = ['Edwina', 'Amphitrite', 'Eric', 'Tristan', 'Drusilla', 'Aidon', 'Celestina', 'Deimos', 'Cosmo', 'Emerick', 'Thanatos', 'Ambrosine', 'Apollo', 'Titania', 'Favian', 'Vivian', 'Lucius', 'Eros', 'Elodie', 'Miriel', 'Helios']
-	defaults = ['Edwina', 'Vivian', 'Emerick', 'Amphitrite', 'Cosmo', 'Tristan']
-else: 
-	data = qod
-	names = ['Persephone', 'Hades', 'Demeter', 'Zeus', 'Hecate', 'Athena', 'Artemis', 'Thanatos', 'Poseidon', 'Charon', 'Cerberus']
-	defaults = ['Persephone', 'Hades', 'Demeter', 'Zeus', 'Hecate', 'Artemis']
-
-load_random(data)
+st.title('sentifluent.') 
+st.markdown('A dashboard for viewing story stats and character targeted sentiment analysis.')
+st.sidebar.markdown('[![Github Badge](https://img.shields.io/badge/-github-grey?style=for-the-badge&logo=github&logoColor=white&link=https://github.com/rubyruins/)](https://www.github.com/rubyruins/) [![Wattpad Badge](https://img.shields.io/badge/-wattpad-orange?style=for-the-badge&logo=wattpad&logoColor=white&link=https://www.wattpad.com/user/rubyruins)](https://www.wattpad.com/user/rubyruins)')
+st.sidebar.title('Pick a story.')
+story = st.sidebar.selectbox('Story', ['Crown of Glass', 'Queen of Death'])
+data, names, defaults = load_data(story)
+load_random()
 
 st.header("Let's see how the readers feel about particular characters.")
 st.subheader("Get started by picking a character from the story. ")
 st.markdown("💡 Pro tip: Switch between different stories in the sidebar!")
-person = st.selectbox('Select', names)
-
-temp = data[data['Text'].str.contains(person.lower())]
-temp = sentiments_to_chapter(temp)
-temp = temp.sum(axis = 0).reset_index()[:3]
-temp.rename(columns ={'index': 'Sentiment', 0: 'Comments'}, inplace = True)
-n = list(temp.Sentiment.values)
-v = list(temp.Comments.values)
-c = ['#636EFA', '#00CC96','#EF553B']
-v, n, c = [list(i) for i in zip(*sorted(zip(v, n, c), reverse=True))]
-st.plotly_chart(px.pie(values = v, names = n, color_discrete_sequence = c, hole=0.5, title=f"Most reader sentiment towards {person} is {n[v.index(max(v))].lower()}.").update_layout(width=600, height=400))
+character = st.selectbox('Select', names)
+labels, values, colors = person_to_sentiments(character)
+st.plotly_chart(px.pie(values = values, names = labels, color_discrete_sequence = colors, hole=0.5, title=f"Most reader sentiment towards {character} is {labels[values.index(max(values))].lower()}.").update_layout(width=600, height=400))
 
 st.subheader("Compare a few of these characters together... ")
-people = st.multiselect('Select', names, default = defaults)
-temp = pd.DataFrame(columns = ['Character', 'Positive', 'Neutral', 'Negative'])
-for person in people:
-	t = data[data['Text'].str.contains(person.lower())]
-	t = sentiments_to_chapter(t)
-	t = t.sum(axis = 0)
-	row = pd.Series(list([person, t.Positive, t.Neutral, t.Negative]), index=temp.columns)
-	temp = temp.append(row, ignore_index=True) 
-temp[['Positive', 'Neutral', 'Negative']] = temp[['Positive', 'Neutral', 'Negative']].div(temp[['Positive', 'Neutral', 'Negative']].sum(axis=1), axis=0) * 100
-temp[['Positive', 'Neutral', 'Negative']] = round(temp[['Positive', 'Neutral', 'Negative']], 2).astype(str) + '%' 
-temp = pd.melt(temp, id_vars=['Character'], value_vars=['Positive', 'Negative', 'Neutral'])
-temp.rename(columns ={'variable': 'Sentiment', 'value': '% Comments'}, inplace = True)
+characters = st.multiselect('Select', names, default = defaults)
+temp = people_to_sentiments(characters)
 st.plotly_chart(px.bar(temp, x = 'Character', y = '% Comments', color='Sentiment', barmode='group'))
 neg_ind = temp[temp.Sentiment == 'Negative']['% Comments'].str[:-1].astype('float').idxmax(axis = 1)
 pos_ind = temp[temp.Sentiment == 'Positive']['% Comments'].str[:-1].astype('float').idxmax(axis = 1)
@@ -133,8 +146,6 @@ st.subheader("Stats over all parts.")
 option = st.radio("Type", ['Comments', 'Reads', 'Votes'], key = 2)
 temp = data[['Comments', 'Reads', 'Votes', 'Part']].drop_duplicates().groupby(by=["Part"])[option].sum().reset_index()
 st.plotly_chart(px.pie(temp, names = 'Part', values = option, hole=0.5).update_layout(width=400, height=400))
-# a, b = list(temp[option].index), list(temp[option].values)
-# st.write(f"Part {a[(b).index(max(b))]} of {story} seems to be the most popular with a total of over {max(b)} {option.lower()}.")
 
 st.subheader("Comments over all time.")
 temp = dict()
